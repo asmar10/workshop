2f0adf12da6b6f2bb07288f50cae09c5075d8899
Syed Asmar Hasan <asmarhasan14@gmail.com>
2025-01-19 07:00:15 +0500

    Update Form.jsx

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -4,18 +4,9 @@ import { ConnectButton } from "thirdweb/react";
 import { client, vanguard } from "../client";
 
 const Form = () => {
-  const [ethAddress, setEthAddress] = useState("");
   const [number, setNumber] = useState("");
 
-  const isValidEthAddress = (address) => /^0x[a-fA-F0-9]{40}$/.test(address);
-
-  const handleEthSubmit = (address) => {
-    if (isValidEthAddress(address)) {
-      alert(`Ethereum address submitted: ${address}`);
-    } else {
-      alert("Invalid Ethereum address!");
-    }
-  };
+  const handleEthSubmit = () => {};
 
   const handleNumberSubmit = () => {
     if (number) {
@@ -28,29 +19,9 @@ const Form = () => {
   return (
     <>
       <h1>Register here: </h1>
-      <input
-        type="text"
-        value={ethAddress}
-        onChange={(e) => setEthAddress(e.target.value)}
-        placeholder="Enter Ethereum address"
-        style={{
-          padding: "12px",
-          fontSize: "16px",
-          borderRadius: "8px",
-          border: "1px solid #ccc",
-          width: "70%",
-          marginBottom: "10px",
-        }}
-      />
-      {ethAddress !== "" && (
-        <p style={{ color: isValidEthAddress(ethAddress) ? "green" : "red" }}>
-          {isValidEthAddress(ethAddress)
-            ? "Valid Ethereum address"
-            : "Invalid Ethereum address"}
-        </p>
-      )}
+
       <button
-        onClick={() => handleEthSubmit(ethAddress)}
+        onClick={() => handleEthSubmit()}
         style={{
           padding: "12px 20px",
           fontSize: "16px",
@@ -66,20 +37,7 @@ const Form = () => {
       </button>
 
       <h1>Guess the imposter:</h1>
-      <input
-        type="number"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-        placeholder="Enter ID"
-        style={{
-          padding: "12px",
-          fontSize: "16px",
-          borderRadius: "8px",
-          border: "1px solid #ccc",
-          width: "50%",
-          marginBottom: "10px",
-        }}
-      />
+
       <button
         onClick={handleNumberSubmit}
         style={{
